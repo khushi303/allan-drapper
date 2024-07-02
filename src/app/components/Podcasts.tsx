b552b9e0e6269d9d151e1348bcf35b9cf8f8976b
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-07-02 10:46:09 +0530

    added h2

---
+++ src/app/components/Podcasts.tsx
@@ -0,0 +1,13 @@
+import { H2 } from "./common/Heading";
+
+const Podcasts = () => {
+  return (
+    <div className="w-full px-3 pt-[154px]">
+      <div className="container mx-auto">
+          <H2 className="text-center" heading="Podcasts"/>
+      </div>
+    </div>
+  );
+};
+
+export default Podcasts;
