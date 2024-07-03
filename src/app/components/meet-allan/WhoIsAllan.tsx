2301b09a6dcdd59aa3be602452abc0f8da400e86
khushi303 <maankhushi303@gmail.com>
2024-07-03 10:58:48 +0530

    merging branch

--- src/app/components/meet-allan/WhoIsAllan.tsx
+++ src/app/components/meet-allan/WhoIsAllan.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { H2 } from "../common/Heading";
 
 const WhoIsAllan = () => {
   return (
     <div className="container xl:max-w-[1172px]">
-      <div className="flex w-4/12"></div>
+      <div className="flex w-4/12">
+        <H2
+          className="inline-block mx-auto relative after:absolute pb-5 after:bottom-0 after:h-[5px] after:bg-chileanFire after:w-16 after:left-1/2 after:-translate-x-1/2"
+          heading="Podcasts"
+        />
+      </div>
     </div>
   );
 };
