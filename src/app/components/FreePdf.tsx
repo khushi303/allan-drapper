47730fa8e0bcd9457bfaf5b74693adb1b661adf1
khushi303 <maankhushi303@gmail.com>
2024-07-03 15:16:51 +0530

    change in text

--- src/app/components/FreePdf.tsx
+++ src/app/components/FreePdf.tsx
@@ -5,7 +5,7 @@ const FreePdf = () => {
   return (
     <div className="container xl:max-w-[1172px]">
       <H3 heading="Grab Your Free PDF Now" className="text-center" />
-      <p>
+      <p className="text-2xl font-medium ">
         The compete entrepreneur’s guide to building a successful business. Sign
         up with your email address to receive weekly resources and tips to help
         scale your business.
