3c5e4e4fcfc7126154a13f54af076f4a70a53203
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-07-03 14:58:55 +0530

    update books

--- src/app/components/Books.tsx
+++ src/app/components/Books.tsx
@@ -1,16 +1,45 @@
+"use client";
 import BooksCardLayout from "./BooksCardLayout";
 import { booksList } from "./common/Helper";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { SwiperArrow } from "./common/Icons";
 
 const Books = () => {
+  const settings = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+    },
+    Navigation: {
+      nextEl: ".swiper_next",
+      prevEl: ".swiper_prev",
+    },
+  };
   return (
-    <div className="px-3">
+    <div className="px-4">
       <div className="container mx-auto px-0 max-w-[1140px]">
         {booksList.map((obj, index) => (
-          <div key={index} className="mt-[60px]">
-            <p className="font-bold text-bunker text-2xl sm:text-custom-xl md:text-custom-2xl !leading-105">
-              {obj.title}
-            </p>
-            <div className="grid grid-cols-3 gap-6">
+          <div key={index} className="mt-7 sm:mt-12 md:mt-14 lg:mt-[60px]">
+            <div className="flex items-center justify-between gap-10">
+              <p className="font-bold text-bunker text-2xl sm:text-custom-xl md:text-custom-2xl !leading-105">
+                {obj.title}
+              </p>
+              <div className="flex gap-1 md:hidden">
+                <button className="swiper_prev px-1 border border-bunker rounded-full">
+                  <SwiperArrow />
+                </button>
+                <button className="swiper_next rotate-180 px-1 border border-bunker rounded-full">
+                  <SwiperArrow />
+                </button>
+              </div>
+            </div>
+            <div className="hidden md:grid grid-cols-3 gap-6 md:my-6 lg:mt-[30px]">
               {obj.books.map((value, index) => (
                 <BooksCardLayout
                   key={index}
@@ -20,6 +49,20 @@ const Books = () => {
                 />
               ))}
             </div>
+            <Swiper {...settings} className="md:!hidden my-3 sm:my-6">
+              {obj.books.map((value, index) => (
+                <SwiperSlide key={index} className="!h-auto">
+                  <div className="h-full">
+                    <BooksCardLayout
+                      key={index}
+                      title={value.title}
+                      author={value.author}
+                      image={value.image}
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         ))}
       </div>
--- src/app/components/BooksCardLayout.tsx
+++ src/app/components/BooksCardLayout.tsx
@@ -6,9 +6,9 @@ interface BooksCard {
 }
 const BooksCardLayout = ({ title, author, image }: BooksCard) => {
   return (
-    <div className="border border-bunker py-[57px] px-12 flex flex-col items-center justify-center mt-[30px]">
-      <Image src={image} alt={title} width={145} height={181} />
-      <p className="mt-2 sm:mt-3 md:mt-4 text-bunker text-opacity-80 font-bold text-xl sm:text-2xl md:text-custom-xl !leading-[120%]">
+    <div className="border border-bunker h-full xl:min-h-[376px] py-9 px-12 flex flex-col items-center justify-center">
+      <Image src={image} alt={title} width={145} height={181}/>
+      <p className="mt-2 text-center sm:mt-3 md:mt-4 text-bunker text-opacity-80 font-bold text-xl sm:text-2xl md:text-custom-xl !leading-[120%]">
         -{title}
       </p>
       <p className="font-medium text-lg sm:text-xl md:text-2xl text-gray">
