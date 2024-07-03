e792cba7d13bb9c1ee2d1df3fad4edacc3bbf867
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-07-03 10:44:19 +0530

    update podcast

--- src/app/components/common/Helper.tsx
+++ src/app/components/common/Helper.tsx
@@ -50,3 +50,39 @@ export const principlesList = [
       "You are not limited by your abilities you are limited by your thinking",
   },
 ];
+
+// ===============================POSCAST DATA LIST===============================
+export const podcastsList = [
+  {
+    thamnail: "/assets/images/png/podcast_thamnal_1.png",
+    title: "Leadership Lessons From Legendary… College Coaches",
+    description: (
+      <>
+        In today's episode, ESPN storyteller & interviewer Marty Smith chats
+        with Allan about his new book, SIDELINE CEO: LEADERSHIP PRINCIPLES FROM…
+      </>
+    ),
+  },
+  {
+    thamnail: "/assets/images/png/podcast_thamnal_2.png",
+    title: "Lead Generation Secrets For Success",
+    description: (
+      <>
+        In today's episode, Allan speaks with Philip Smith, a serial
+        entrepreneur, investor, and author. Smith has made the Inc. 5000 list of
+        fastest growing private
+      </>
+    ),
+  },
+  {
+    thamnail: "/assets/images/png/podcast_thamnal_1.png",
+    title: "From ‘Weakness’ To Superhero Skill",
+    description: (
+      <>
+        In Today's episode, Allan speaks with multi platinum record producing DJ
+        and Telzio Founder & CEO Peter Schroeder. Topics include: 1:22 Surviving
+        a literal…
+      </>
+    ),
+  },
+];
