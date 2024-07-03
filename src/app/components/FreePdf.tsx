c23d770d1258d19eedce7d44b84d5efc5356e3e0
khushi303 <maankhushi303@gmail.com>
2024-07-03 15:11:26 +0530

    change in text

--- src/app/components/FreePdf.tsx
+++ src/app/components/FreePdf.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { H3 } from "./common/Heading";
 
 const FreePdf = () => {
-            return <div>
-              
-  </div>;
+  return (
+    <div className="container xl:max-w-[1172px]">
+      <H3 heading="Grab Your Free PDF Now" className="text-center" />
+      <p>
+        The compete entrepreneur’s guide to building a successful business. Sign
+        up with your email address to receive weekly resources and tips to help
+        scale your business.
+      </p>
+    </div>
+  );
 };
 
 export default FreePdf;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import FreePdf from "./components/FreePdf";
 
 export const metadata: Metadata = {
   title: "Allan Drapper",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <FreePdf />
       </body>
     </html>
   );
